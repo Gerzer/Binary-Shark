c405ffa3eab411660390332798a07a4696bccb3e
Gerzer <gerzersoftware@icloud.com>
2018-01-26 09:24:14 -0500

    Initial Commit

---
+++ Binary Shark/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Binary Shark
+//
+//  Created by Gabriel Jacoby-Cooper on 1/26/18.
+//  Copyright © 2018 Gerzer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
